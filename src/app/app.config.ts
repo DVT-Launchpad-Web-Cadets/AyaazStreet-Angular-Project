70787ccc8554d095f59af875a8e71c9f174af547
Ayaaz Street <astreet@dvtsoftware.com>
2024-04-26 09:48:18 +0200

    feat: :construction: Connected Firebase authentication
    Addedd Firebase tho app.config and created authentication service.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
+import {getAuth, provideAuth} from "@angular/fire/auth";
+import {getAnalytics} from "@angular/fire/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAtn-g4uiyo9-EFTIPqKgZfOGf3nOSUO_0",
+    authDomain: "diamu-ayz14.firebaseapp.com",
+    projectId: "diamu-ayz14",
+    storageBucket: "diamu-ayz14.appspot.com",
+    messagingSenderId: "191442399326",
+    appId: "1:191442399326:web:2a0ee170cfe6165f96c4cf",
+    measurementId: "G-GR1F161BB5"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+    providers: [provideRouter(routes), importProvidersFrom([provideFirebaseApp(() => app), provideAuth(() => getAuth())])]
 };
