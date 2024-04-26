72a0cf16a573d02f073a8ac4fabb2fabe5bf14a7
Ayaaz Street <astreet@dvtsoftware.com>
2024-04-26 14:37:56 +0200

    build: :green_heart: Created GitHub workflow
    Added workflow to build and host application when merged. Added secrets for API key and added environment variables.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,20 +4,8 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
 import {getAuth, provideAuth} from "@angular/fire/auth";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAtn-g4uiyo9-EFTIPqKgZfOGf3nOSUO_0",
-    authDomain: "diamu-ayz14.firebaseapp.com",
-    projectId: "diamu-ayz14",
-    storageBucket: "diamu-ayz14.appspot.com",
-    messagingSenderId: "191442399326",
-    appId: "1:191442399326:web:2a0ee170cfe6165f96c4cf",
-    measurementId: "G-GR1F161BB5"
-};
-
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+import {environment} from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), importProvidersFrom([provideFirebaseApp(() => initializeApp(firebaseConfig)), provideAuth(() => getAuth())])]
+    providers: [provideRouter(routes), importProvidersFrom([provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), provideAuth(() => getAuth())])]
 };
