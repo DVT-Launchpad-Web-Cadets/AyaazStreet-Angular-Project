20b8d505428a0c29dd365dd1d82977e47dbc7d24
Ayaaz Street <astreet@dvtsoftware.com>
2024-05-03 01:21:31 +0200

    style: :iphone: Edited responsive layout
    Changed md flex from row to col

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,22 @@
-import { Routes } from "@angular/router";
-import { LandingPageComponent } from "./components/landing-page/landing-page.component";
-import { LoginPageComponent } from "./components/login-page/login-page.component";
-import { RegisterPageComponent } from "./components/register-page/register-page.component";
+import {Routes} from "@angular/router";
+import {LandingPageComponent} from "./components/landing-page/landing-page.component";
+import {LoginPageComponent} from "./components/login-page/login-page.component";
+import {RegisterPageComponent} from "./components/register-page/register-page.component";
 
 export const routes: Routes = [
-	{
-		path: "",
-		component: LandingPageComponent,
-	},
-	{
-		path: "login",
-		component: LoginPageComponent,
-	},
-	{
-		path: "register",
-		component: RegisterPageComponent,
-	},
+    {
+        path: "",
+        component: LandingPageComponent,
+        title: "Home | Diamu"
+    },
+    {
+        path: "login",
+        component: LoginPageComponent,
+        title: "Login | Diamu"
+    },
+    {
+        path: "register",
+        component: RegisterPageComponent,
+        title: "Register | Diamu"
+    },
 ];
